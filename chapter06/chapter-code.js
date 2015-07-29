fbfcd9c8d14c31f2147b3148336c5b3914768c6e
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-07-28 21:30:59 -0500

    More code from chapter.

--- chapter06/chapter-code.js
+++ chapter06/chapter-code.js
@@ -14,7 +14,62 @@ var fatRabbit = { type: "fat", speak: speak };
 whiteRabbit.speak("Oh my ears and whiskers, how late it's getting!");
 fatRabbit.speak("I could sure use a carrot right now.");
 speak.apply(fatRabbit, ["Burp!"]);
-speak.call({type: "old"}, "Oh my.")
+speak.call({type: "old"}, "Oh my.");
+
+// Prototype
+var protoRabbit = {
+  speak: function(line) {
+    console.log("The " + this.type + " rabbit says '" + line + "'");
+  }
+};
+var killerRabit = Object.create(protoRabbit);
+killerRabit.type = "killer";
+killerRabit.speak("SKREEEE!");
+
+// Constructors
+function Rabbit(type) {
+  this.type = type;
+}
+
+var killerRabit = new Rabbit("killer");
+var blackRabbit = new Rabbit("black");
+console.log(blackRabbit.type);
+
+Rabbit.prototype.speak = function (line) {
+  console.log("The " + this.type + " rabbit says '" + line + "'");
+};
+blackRabbit.speak("Doom...");
+
+// Prototype interference
+Rabbit.prototype.dance = function() {
+  console.log("The " + this.type + " rabbit dances a jig.");
+};
+killerRabit.dance();
+
+var map = {foo: 0, bar:  1};
+Object.defineProperty(Object.prototype, "hiddenNonsense", { enumerable: false, value: "hi"});
+for (var name in map)
+  console.log(name);
+console.log(map.hiddenNonsense);
+
+// Prototype-less objects
+
+var map = Object.create(null);
+map["pizza"] = 0.069;
+console.log("toString" in map);
+console.log("pizza" in map);
+
+// Polymorphism
+
+var MOUNTAINS = [
+  {name: "Kilimanjaro", height: 5895, country: "Tanzania"},
+  {name: "Everest", height: 8848, country: "Nepal"},
+  {name: "Mount Fuji", height: 3776, country: "Japan"},
+  {name: "Mont Blanc", height: 4808, country: "Italy/France"},
+  {name: "Vaalserberg", height: 323, country: "Netherlands"},
+  {name: "Denali", height: 6168, country: "United States"},
+  {name: "Popocatepetl", height: 5465, country: "Mexico"}
+];
 
 function rowHeights(rows) {
   return rows.map(function(row) {
@@ -93,3 +148,31 @@ for (var i = 0; i < 5; i++) {
   rows.push(row);
 }
 console.log(drawTable(rows));
+
+function UnderlinedCell(inner) {
+  this.inner = inner;
+};
+UnderlinedCell.prototype.minWidth = function() {
+  return this.inner.minWidth();
+};
+UnderlinedCell.prototype.minHeight = function() {
+  return this.inner.minHeight() + 1;
+};
+UnderlinedCell.prototype.draw = function(width, height) {
+  return this.inner.draw(width, height - 1).concat([repeat("-", width)]);
+}
+
+function dataTable(data) {
+  var keys = Object.keys(data[0]);
+  var headers = keys.map(function (name) {
+    return new UnderlinedCell(new TextCell(name));
+  });
+  var body = data.map(function(row) {
+    return keys.map(function(name) {
+      return new TextCell(String(row[name]));
+    });
+  });
+  return[headers].concat(body);
+}
+
+console.log(drawTable(dataTable(MOUNTAINS)));
