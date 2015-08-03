4829b527505a2c6a277576b830deaf204b401112
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-08-02 21:55:02 -0500

    Added WallFollower critter

--- chapter07/elife.js
+++ chapter07/elife.js
@@ -178,3 +178,22 @@ for (var i = 0; i < 5; i++) {
   world.turn();
   console.log(world.toString());
 }
+
+function dirPlus(dir, n) {
+  var index = directionNames.indexOf(dir);
+  return directionNames[(index + n  + 8) % 8];
+}
+
+function WallFollower() {
+  this.dir = "s";
+}
+WallFollower.prototype.act = function(view) {
+  var start = this.dir;
+  if (view.look(dirPlus(this.dir, -3)) != " ")
+    start = this.dir = dirPlus(this.dir, -2);
+  while (view.look(this.dir) != " ") {
+    this.dir = dirPlus(this.dir, 1);
+    if (this.dir == start) break;
+  }
+  return {type: "move", direction: this.dir};
+}
