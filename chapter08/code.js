0ae39d7b5568ef1250e2d3a432cf1f75206865d6
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-08-04 22:13:28 -0500

    Finished chapter 08

--- chapter08/code.js
+++ chapter08/code.js
@@ -6,3 +6,95 @@ function canYouSpotTheProblem() {
 }
 
 //canYouSpotTheProblem();
+
+function Vector(x, y) {
+  this.x = x;
+  this.y = y;
+}
+Vector.prototype.plus = function(other) {
+  return new Vector(this.x + other.x, this.y + other.y);
+}
+
+function testVector() {
+  var p1 = new Vector(10, 20);
+  var p2 = new Vector(-10, 5);
+  var p3 = p1.plus(p2);
+
+  if (p1.x !== 10) return "fail: x property";
+  if (p1.y !== 20) return "fail: y property";
+  if (p2.x !== -10) return "fail: negative x property";
+  if (p3.x !== 0) return "fail: x from plus";
+  if (p3.y !== 25) return "fail: y from plus";
+  return "everything ok";
+}
+console.log(testVector());
+
+
+function numberToString(n, base) { var result = "", sign = "";
+  if (n < 0) {
+    sign = "-";
+    n = -n;
+  }
+  do {
+    result = String(n % base) + result;
+    n = Math.floor(n / base);
+  } while (n > 0);
+  return sign + result;
+}
+console.log(numberToString(13, 10));
+
+function propmtDirection(question) {
+  var result = prompt(question, "");
+  if (result.toLowerCase() == "left") return "L";
+  if (result.toLowerCase() == "right") return "R";
+  throw new Error("Invalid direction: " + result);
+}
+
+function look() {
+  if (propmtDirection("Which way?") == "L")
+    return "a house";
+  else {
+    return "two angry bears";
+  }
+}
+
+try {
+  console.log("You see", look());
+} catch (error) {
+  console.log("Something went wrong: " + error);
+}
+
+var box = {
+  locked: true,
+  unlock: function() { this.locked = false; },
+  lock: function() { this.locked = true; },
+  _content: [],
+  get content () {
+    if (this.locked) throw new Error("Locked!");
+    return this._content;
+  }
+};
+
+
+function withBoxUnlocked(body) {
+  box.unlock();
+  try {
+    return body();
+  }  finally {
+    box.lock();
+  }
+}
+
+withBoxUnlocked(function() {
+  box.content.push("gold piece");
+});
+
+try {
+  withBoxUnlocked(function() {
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+} catch (e) {
+  console.log("Error raised:", e);
+}
+console.log(box.locked);
+// → true
