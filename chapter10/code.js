13d9ef467f743ee84da892f770876b7b8bafe56c
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-08-10 21:08:39 -0500

    Using module pattern.

--- chapter10/code.js
+++ chapter10/code.js
@@ -16,3 +16,15 @@ var weekDay = function() {
   };
 }();
 console.log(weekDay.name(weekDay.number("Sunday")));
+
+(function(exports) {
+  var names = ["Sunday", "Monday", "Tuesday", "Wednesday",
+               "Thursday", "Friday", "Saturday"];
+  exports.name = function(number) {
+    return names[number];
+  };
+  exports.number = function(name) {
+    return names.indexOf(name);
+  };
+})(this.weekDay = {});
+console.log(weekDay.name(weekDay.number("Saturday")));
