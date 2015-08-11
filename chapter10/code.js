c8abbc901e06112f0f67094d05389070af92be41
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-08-10 21:05:23 -0500

    Fixed line ending issue.

--- chapter10/code.js
+++ chapter10/code.js
@@ -1,1 +1,18 @@
-var dayName = function() {  var names = ["Sunday", "Monday", "Tuesday", "Wednesday",               "Thursday", "Friday", "Saturday"];  return function(number) {    return names[number];  }}();console.log(dayName(1));var weekDay = function() {  var names = ["Sunday", "Monday", "Tuesday", "Wednesday",               "Thursday", "Friday", "Saturday"];  return {    name: function(number) { return names[number]; },    number: function(name) { return names.indexOf(name); }  };}();console.log(weekDay.name(weekDay.number("Sunday")));
+var dayName = function() {
+  var names = ["Sunday", "Monday", "Tuesday", "Wednesday",
+               "Thursday", "Friday", "Saturday"];
+  return function(number) {
+    return names[number];
+  }
+}();
+console.log(dayName(1));
+
+var weekDay = function() {
+  var names = ["Sunday", "Monday", "Tuesday", "Wednesday",
+               "Thursday", "Friday", "Saturday"];
+  return {
+    name: function(number) { return names[number]; },
+    number: function(name) { return names.indexOf(name); }
+  };
+}();
+console.log(weekDay.name(weekDay.number("Sunday")));
