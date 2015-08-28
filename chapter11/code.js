d85ce9fab79207d83d122d404e5c5819e0fe96c1
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-08-27 21:35:37 -0500

    Add evaluate function and if special form function.

--- chapter11/code.js
+++ chapter11/code.js
@@ -45,4 +45,39 @@ function parse(program) {
   return result.expr;
 }
 
-console.log(parse("+(a, 10)"))
+console.log(parse("+(a, 10)"));
+
+function evaluate(expr, env) {
+  switch (expr.type) {
+    case "value":
+      return expr.value;
+    case "word":
+      if (expr.name in env)
+        return env[expr.name];
+      else
+        throw new ReferenceError("Undefined variable: " + expr.name);
+    case "apply":
+      if (expr.operator.type == "word" &&
+          expr.operator.name in specialForms)
+        return specialForms[expr.operator.name](expr.args, env)
+
+      var op = evaluate(expr.operator, env);
+      if (typeof op != "function")
+        throw new TypeError("Applying a non-function.");
+      return op.apply(null, expr.args.map(function(arg) {
+        return evaluate(arg, env);
+      }));
+  }
+}
+
+var specialForms = Object.create(null);
+
+specialForms["if"] = function(args, env) {
+  if (args.length != 3)
+    throw new SyntaxError("Bad number of args to if");
+
+    if (evaluate(args[0], env) !== false)
+      return evaluate(args[1], env);
+    else
+      return evaluate(args[2], env);
+};
