c40b5b971fb69ce5c384fb8c2d2e656fd7915a9f
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-08-30 19:17:24 -0500

    Add examples of defining functions

--- chapter11/code.js
+++ chapter11/code.js
@@ -163,3 +163,12 @@ specialForms["fun"] = function(args, env) {
     return evaluate(body, localEnv);
   };
 };
+
+run("do(define(plusOne, fun(a, +(a, 1))),",
+    "   print(plusOne(10)))");
+
+run("do(define(pow, fun(base, exp,",
+    "     if(==(exp, 0),",
+    "        1,",
+    "        *(base, pow(base, -(exp, 1)))))),",
+    "   print(pow(2, 10)))");
