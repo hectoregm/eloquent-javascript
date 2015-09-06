e22420a94924300ae12eb63f818b60b34d871c7f
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-09-05 21:43:05 -0500

    Implement elt.

--- chapter12/code.js
+++ chapter12/code.js
@@ -20,3 +20,15 @@ function replaceImages() {
     }
   }
 }
+
+function elt(type) {
+  var node = document.createElement(type);
+  for (var i = 1; i < arguments.length; i++) {
+    var child = argument[i];
+    if (typeof child == "string")
+      child = document.createTextNode(child);
+    node.appendChild(child);
+  }
+
+  return node;
+}
