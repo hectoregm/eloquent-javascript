21dda31fadf50cf763b72fd860233d39eada6990
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-09-06 22:03:05 -0500

    Code from chapter 14.

---
+++ chapter14/code.js
@@ -0,0 +1,8 @@
+var button = document.querySelector("button");
+
+function once() {
+  console.log("Done.");
+  button.removeEventListener("click", once);
+}
+
+button.addEventListener("click", done);
