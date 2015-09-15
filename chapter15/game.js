e1f01a5468f9961845409805306d9d4daf494343
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-09-14 20:57:01 -0500

    Add Vector class.

--- chapter15/game.js
+++ chapter15/game.js
@@ -37,3 +37,19 @@ function Level(plan) {
   })[0];
   this.status = this.finishDelay = null;
 }
+
+Level.prototype.isFinished = function() {
+  return this.status != null && this.finishDelay < 0;
+};
+
+function Vector(x, y) {
+  this.x = x; this.y = y;
+}
+
+Vector.prototype.plus = function(other) {
+  return new Vector(this.x + other.x, this.y + other.y);
+};
+
+Vector.prototype.times = function(factor) {
+  return new Vector(this.x * factor, this.y * factor);
+}
