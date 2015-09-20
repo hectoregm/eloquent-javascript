279d3cf9617beb9d7c94c110b49f1716d8046a4f
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-09-19 21:56:11 -0500

    Draw background add css.

--- chapter15/game.js
+++ chapter15/game.js
@@ -104,3 +104,18 @@ function DOMDisplay(parent, level) {
   this.actorLayer = null;
   this.drawFrame();
 }
+
+var scale = 20;
+
+DOMDisplay.prototype.drawBackground = function() {
+  var table = elt("table", "background");
+  table.style.width = this.level.width * scale + "px";
+  this.level.grid.forEach(function(row) {
+    var rowElt = table.appendChild(elt("tr"));
+    rowElt.style.height = scale + "px";
+    row.forEach(function(type) {
+      rowElt.appendChild(elt("td"), type);
+    });
+  });
+  return table;
+}
