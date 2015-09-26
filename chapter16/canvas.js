26e80afd1126e65ab8b6ea636704e9acc566c256
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-09-26 08:26:50 -0500

    First implementation of CanvasDisplay

--- chapter16/canvas.js
+++ chapter16/canvas.js
@@ -1,3 +1,5 @@
+var scale = 20;
+
 function CanvasDisplay(parent, level) {
   this.canvas = document.createElement("canvas");
   this.canvas.width = Math.min(600, level.width * scale);
@@ -12,8 +14,8 @@ function CanvasDisplay(parent, level) {
   this.viewport = {
     left: 0,
     top: 0,
-    width: this.canvas.width / scale;
-    height: this.canvas.height / scale;
+    width: this.canvas.width / scale,
+    height: this.canvas.height / scale
   };
 
   this.drawFrame(0);
@@ -29,4 +31,54 @@ CanvasDisplay.prototype.drawFrame = function(step) {
   this.clearDisplay();
   this.drawBackground();
   this.drawActors();
+}
+
+CanvasDisplay.prototype.updateViewport = function() {
+  var view = this.viewport, margin = view.width / 3;
+  var player = this.level.player;
+  var center = player.pos.plus(player.size.times(0.5));
+
+  if (center.x < view.left + margin)
+    view.left = Math.max(center.x - margin, 0);
+  else if (center.x > view.left + view.width - margin)
+    view.left = Math.min(center.x + margin - view.width,
+                         this.level.width - view.width);
+
+  if (center.y < view.top + margin)
+    view.top = Math.max(center.y - margin, 0);
+  else if (center.y > view.top + view.height - margin)
+    view.top = Math.min(center.y + margin - view.height,
+                        this.level.height - view.height);
+};
+
+CanvasDisplay.prototype.clearDisplay = function() {
+  if (this.level.status == "won")
+    this.cx.fillStyle = "rgb(68, 191, 255)";
+  else if (this.level.status == "lost")
+    this.cx.fillStyle = "rgb(44, 136, 214)";
+  else
+    this.cx.fillStyle = "rgb(52, 166, 251)";
+  this.cx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+};
+
+var otherSprites = document.createElement("img");
+otherSprites.src = "img/sprites.png";
+
+CanvasDisplay.prototype.drawBackground = function() {
+  var view = this.viewport;
+  var xStart = Math.floor(view.left);
+  var xEnd = Math.ceil(view.left + view.width);
+  var yStart = Math.floor(view.top);
+  var yEnd = Math.ceil(view.top + view.height);
+
+  for (var y = yStart; y < yEnd; y++) {
+    for (var x = xStart; x < xEnd; x++) {
+      var tile = this.level.grid[y][x];
+      if (tile == null) continue;
+      var screenX = (x - view.left) * scale;
+      var screenY = (x - view.top) * scale;
+      var tileX = tile == "lava" ? scale : 0;
+      this.cx.drawImage(otherSprites, tileX, 0, scale, scale, screenX, screenY, scale, scale)
+    }
+  }
 }
