aede7efe0323194b64c0f4d637b7b5958ac698e7
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-09-28 08:52:04 -0500

    AJAX requests

--- chapter17/code.js
+++ chapter17/code.js
@@ -1,2 +1,16 @@
 console.log(encodeURIComponent("Hello & goodbye"));
 console.log(decodeURIComponent("Hello%20%26%20goodbye"));
+
+var req = new XMLHttpRequest();
+req.open("GET", "example/data.txt", false);
+req.send(null);
+console.log(req.responseText);
+console.log(req.status, req.statusText);
+console.log(req.getResponseHeader("content-type"));
+
+// Async requests
+req = new XMLHttpRequest();
+req.open("GET", "example/data.txt", true);
+req.addEventListener("load", function() {
+  console.log("Done: ", req.status);
+});
